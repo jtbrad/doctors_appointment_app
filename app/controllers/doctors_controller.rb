15fde963fe91fc9a5944864f4f5ee1b1b5edf44e
Jacob Bradford <jcbbrdfrd@yahoo.com>
2019-11-19 15:29:23 -0700

    added functionality to create new doctors

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,11 +1,25 @@
 class DoctorsController < ApplicationController
+
   def index
+    @doctors = Doctor.all
   end
 
   def show
   end
 
   def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.new(params.require(:doctor).permit(:name))
+
+    if @doctor.save
+      redirect_to doctors_path
+    else
+      render :new
+    end
+
   end
 
   def edit
